8f4f9aa802e710f83c5ecb57e4c1cac1514a8f5b
matheusbach <math.bach.com@gmail.com>
2021-12-09 11:06:05 -0300

    enable kawpow build

--- build-gcc-win32.sh
+++ build-gcc-win32.sh
@@ -12,7 +12,7 @@ rm -rf build-gcc-win32
 mkdir build-gcc-win32
 cd build-gcc-win32
 
-cmake .. -G "Unix Makefiles" -DXMRIG_DEPS=c:/xmrig-deps/gcc/x86        -DWITH_AEON=OFF -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF -DWITH_CN_FEMTO=OFF -DWITH_ARGON2=OFF -DWITH_ASTROBWT=OFF -DWITH_KAWPOW=OFF
+cmake .. -G "Unix Makefiles" -DXMRIG_DEPS=c:/xmrig-deps/gcc/x86 -DWITH_AEON=OFF -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF -DWITH_CN_FEMTO=OFF -DWITH_ARGON2=OFF -DWITH_ASTROBWT=OFF -DWITH_KAWPOW=ON
 cmake --build . --config Release
 make -j$(nproc)
 
