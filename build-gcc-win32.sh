337b6582b6cb434b438b2dc9c055e18a69ec553e
matheusbach <math.bach.com@gmail.com>
2022-01-06 13:15:26 -0300

    Update scripts

--- build-gcc-win32.sh
+++ build-gcc-win32.sh
@@ -12,7 +12,7 @@ rm -rf build-gcc-win32
 mkdir build-gcc-win32
 cd build-gcc-win32
 
-cmake .. -G "Unix Makefiles" -DXMRIG_DEPS=c:/xmrig-deps/gcc/x86 -DWITH_AEON=OFF -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF -DWITH_CN_FEMTO=OFF -DWITH_ARGON2=ON -DWITH_ASTROBWT=ON -DWITH_KAWPOW=ON
+cmake .. -G "Unix Makefiles" -DXMRIG_DEPS=c:/xmrig-deps/gcc/x86
 cmake --build . --config Release
 make -j$(nproc)
 
--- build-msvc-win64.cmd
+++ build-msvc-win64.cmd
@@ -6,7 +6,7 @@ del /F /S /Q build-msvc-win64
 
 mkdir build-msvc-win64
 cd build-msvc-win64
-cmake .. -G "Visual Studio 16 2019" -A x64 -DXMRIG_DEPS=c:\xmrig-deps\msvc2019\x64 -DWITH_AEON=OFF -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF -DWITH_CN_FEMTO=OFF -DWITH_ARGON2=ON -DWITH_ASTROBWT=ON -DWITH_KAWPOW=ON
+cmake .. -G "Visual Studio 16 2019" -A x64 -DXMRIG_DEPS=c:\xmrig-deps\msvc2019\x64
 cmake --build . --config Release
 
 echo.
